4670bb4e8b814befca79f4b234fdd9d44ba91cf8
Thomaz Ferreira Neves Dos Santos <thomaz.ferreira@mercadolivre.com>
2021-12-17 15:51:14 -0300

    primeiro commit

---
+++ Aula 1/Pratica Integradora II/exercicio_1.java
@@ -0,0 +1,20 @@
+public class exercicio_1{
+    public static void main(String args[]){
+        Integer[] array = {52, 10, 85, 1324, 01, 13, 62, 30, 12, 127};
+        Integer[] sortedArray = new Integer[array.length];
+
+        for(int i=0; i<array.length; i++){
+            int indexPos = 0;
+
+            for(int j=0; j<array.length; j++){
+                if(j == i) continue;
+                if(array[i]>array[j]) indexPos+=1;
+            }
+            sortedArray[indexPos] = array[i];
+        }
+
+        for (int item : sortedArray) {
+            System.out.println(item);
+        }
+    }
+}
