4334431a12fee85ab5b59c3deb4c40d050f7e56f
Thomaz Ferreira Neves Dos Santos <thomaz.ferreira@mercadolivre.com>
2021-12-23 15:26:12 -0300

    alteração estrutura

--- Aula 2 - Java II/Introducao a Java TT/Pratica Integradora I/Exercicio2 - Figura Geometrica/App.java
+++ Aula 2 - Java II/Introducao a Java TT/Pratica Integradora I/Exercicio2 - Figura Geometrica/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Circulo c = new Circulo(2);
-        Retangulo r = new Retangulo(2);
-        Triangulo t = new Triangulo(3, 4);
+        FiguraGeometrica c = new Circulo(2);
+        FiguraGeometrica r = new Retangulo(2);
+        FiguraGeometrica t = new Triangulo(3, 4);
 
         FiguraGeometrica[] figs = new FiguraGeometrica[3];
         figs[0] = c;
